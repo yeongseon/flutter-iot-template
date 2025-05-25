f04cbe99adb6113ffaeff81100d7fcf3e5abf50f
Yeongseon Choe <yeongseon.choe@gmail.com>
2025-05-25 18:42:01 +0900

    feat(edge_client): use BACKEND_HOST and BACKEND_PORT environment variables

--- edge_client/hello_client.py
+++ edge_client/hello_client.py
@@ -1,10 +1,17 @@
+import os
 import requests
 import time
 
+backend_host = os.getenv("BACKEND_HOST", "localhost")
+backend_port = os.getenv("BACKEND_PORT", "8000")
+url = f"http://{backend_host}:{backend_port}/data"
+
+print("Starting edge_client...", flush=True)
+
 while True:
     try:
-        res = requests.post("http://backend:8000/data", json={"temperature": 23.5})
-        print("Sent data:", res.status_code)
+        res = requests.post(url, json={"temperature": 23.5})
+        print("Sent data:", res.status_code, flush=True)
     except Exception as e:
-        print("Error sending data:", e)
+        print("Error sending data:", e, flush=True)
     time.sleep(5)
